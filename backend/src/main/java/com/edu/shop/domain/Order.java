3f3ecaf55dc18728672b3fdff9f2203710b9a860
VanHai2404 <hovanhai24042003@gmai.com>
2023-10-30 20:54:39 +0700

    Commit Entity(domain)_'12/10/2023'

---
+++ backend/src/main/java/com/edu/shop/domain/Order.java
@@ -0,0 +1,67 @@
+package com.edu.shop.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer orderId;
+	@Temporal(TemporalType.DATE)
+	private Date orderDate;
+//	@Column(nullable = false)
+//	private int customerId;
+	@Column(nullable = false)
+	private Double amount;
+	@Column(columnDefinition = "nvarchar(100) not null")
+	private String note;
+
+	@Column(nullable = false)
+	private Short status;
+
+	@ManyToOne
+	@JoinColumn(name = "customerId")
+	private Customer customer;
+
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private PayMent payMent;
+
+	@OneToOne
+	@JoinColumn(name = "addressId")
+	private Address address;
+
+	@OneToOne
+	@JoinColumn(name = "voucher_id")
+	private Voucher voucher;
+
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<OrderDetail> orderDetails;
+
+
+
+}
---
+++ backend/src/main/java/com/edu/shop/domain/OrderDetail.java
@@ -0,0 +1,42 @@
+package com.edu.shop.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "OrderDetails")
+public class OrderDetail implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer orderDetailId;
+//	@Column(nullable = false)
+//	private int orderId;
+	@Column(nullable = false)
+	private Integer quantity;
+	@Column(nullable = false)
+	private Double unitPrice;
+
+	@ManyToOne
+	@JoinColumn(name = "productId")
+	private Product product;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "orderId")
+	private Order order;
+
+}
